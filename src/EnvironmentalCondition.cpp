275a3198f097fffd962a6d06be36dd6f0bbc273e
Verena <Verena@UNICORN>
2017-03-24 23:37:51 +0100

    Teil 1

--- src/EnvironmentalCondition.cpp
+++ src/EnvironmentalCondition.cpp
@@ -11,10 +11,78 @@
 
 #include "EnvironmentalCondition.h"
 
-EnvironmentalCondition::EnvironmentalCondition()
+using namespace std;
+
+EnvironmentalCondition::EnvironmentalCondition(int sky_cover, int precipitation, float temperature, int wind)
 {
-  // TODO Auto-generated constructor stub
-	//Test
+  enum sky_cover: int
+  {
+    SUNNY, CLOUDY, OVERCAST, VERY_OVERCAST
+  };
+  enum precipitation: int
+  {
+    NONE, LOW, MEDIUM, HIGH
+  };
+  enum wind: int
+  {
+    NONE, LOW, MEDIUM, HIGH
+  };
+
+
+  //Methoden
+  void EnvironmentalCondition::isltHot(int sky_cover, int temperature)
+
+
+  void EnvironmentalCondition::isltRainy(int precipitation, int sky_cover)
+
+
+  void EnvironmentalCondition::isltStormy(int wind, int sky_cover)
+
+
+
+
+  public:
+    //Setters
+    void setSkyCover(int cover)
+    {
+      sky_cover = cover;
+    }
+    void setPrecipitation(int rank)
+    {
+      precipitation = rank;
+    }
+    void setTemperature(float temperature)
+    {
+      temperature = temperature;
+    }
+    void setWind(int rank)
+    {
+      wind = rank;
+    }
+
+    //Getters
+    int getSkyCover() const
+    {
+      return void;
+    }
+    int getPrecipitation() const
+    {
+      return void;
+    }
+    int getTemperature() const
+    {
+      return void;
+    }
+    int getWind() const
+    {
+      return void;
+    }
+
+  private:
+    int sky_cover;
+    int precipitation;
+	int temperature;
+	int wind;
 
 }
 
