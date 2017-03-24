7156537e66df0a485ffc6f1149276f196d808cc3
exokortex <johannes.haring@gmx.net>
2017-03-24 14:17:22 +0100

    EnvironmentalEngine added

---
+++ src/EnvironmentalEngine.cpp
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// EnvironmentalCondition.h
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
+
+#include "EnvironmentalEngine.h"
+
+EnvironmentalEngine::EnvironmentalEngine()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/EnvironmentalEngine.h
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// EnvironmentalCondition.h
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
+
+#ifndef ENVIRONMENTALENGINE_H_INCLUDED
+#define ENVIRONMENTALENGINE_H_INCLUDED
+
+class EnvironmentalEngine
+{
+  public:
+    EnvironmentalEngine();
+};
+
+#endif /* ENVIRONMENTALENGINE_H_INCLUDED */
