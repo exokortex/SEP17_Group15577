63fffdbb72db6984cd96ace77d54afee1bd5cf2e
exokortex <johannes.haring@gmx.net>
2017-03-24 14:14:44 +0100

    EnvironmentalCondition added

---
+++ src/EnvironmentalCondition.cpp
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// EnvironmentalCondition.cpp
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
+
+#include "EnvironmentalCondition.h"
+
+EnvironmentalCondition::EnvironmentalCondition()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/EnvironmentalCondition.h
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// EnvironmentalCondition.h
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
+
+#ifndef ENVIRONMENTALCONDITION_H_INCLUDED
+#define ENVIRONMENTALCONDITION_H_INCLUDED
+
+class EnvironmentalCondition
+{
+  public:
+    EnvironmentalCondition();
+};
+
+#endif /* ENVIRONMENTALCONDITION_H_INCLUDED */
