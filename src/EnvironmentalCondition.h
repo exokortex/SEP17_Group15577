860069f8b9aafa842091ae54e771120830dd9e9c
exokortex <johannes.haring@gmx.net>
2017-03-25 14:00:26 +0100

    EnvironmentalCondition code added (no docs), Test main

--- src/EnvironmentalCondition.h
+++ src/EnvironmentalCondition.h
@@ -15,7 +15,68 @@
 class EnvironmentalCondition
 {
   public:
-    EnvironmentalCondition();
+    const int TEMP_MIN = 10;
+    const int TEMP_MAX = 35;
+    const int TEMP_UPPERTHIRD = (TEMP_MAX - TEMP_MIN) / 3. * 2. + TEMP_MIN;
+
+    enum Cover
+    {
+      SUNNY, CLOUDY, OVERCAST, VERY_OVERCAST
+    };
+    enum Rank
+    {
+      NONE, LOW, MEDIUM, HIGH
+    };
+
+    // Methods
+    EnvironmentalCondition(Cover sky_cover, Rank precipitation,
+        float temperature, Rank wind);
+
+    bool isItHot();
+    bool isItRainy();
+    bool isItStormy();
+
+    // Setters
+    void setSkyCover(Cover cover)
+    {
+      sky_cover_ = cover;
+    }
+    void setPrecipitation(Rank rank)
+    {
+      precipitation_ = rank;
+    }
+    void setTemperature(float temperature)
+    {
+      temperature_ = temperature;
+    }
+    void setWind(Rank rank)
+    {
+      wind_ = rank;
+    }
+
+    // Getters
+    Cover getSkyCover() const
+    {
+      return sky_cover_;
+    }
+    Rank getPrecipitation() const
+    {
+      return precipitation_;
+    }
+    float getTemperature() const
+    {
+      return temperature_;
+    }
+    Rank getWind() const
+    {
+      return wind_;
+    }
+
+  private:
+    Cover sky_cover_;
+    Rank precipitation_;
+    float temperature_;
+    Rank wind_;
 };
 
 #endif /* ENVIRONMENTALCONDITION_H_INCLUDED */
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,22 @@
-
 #include <iostream>
+
+#include "EnvironmentalCondition.h"
 using std::cout;
 using std::endl;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl;
-	return 0;
+int main()
+{
+  EnvironmentalCondition ec = EnvironmentalCondition(EnvironmentalCondition::SUNNY,
+      EnvironmentalCondition::HIGH, 34.0, EnvironmentalCondition::MEDIUM);
+
+  cout << "Precipitation: " << ec.getPrecipitation() << endl;
+  cout << "Temperature: " << ec.getTemperature() << endl;
+  ec.setTemperature(33.0);
+  cout << "Temperature: " << ec.getTemperature() << endl;
+
+  cout << "isItHot: " << ec.isItHot() << endl;
+  cout << "isItRainy: " << ec.isItRainy() << endl;
+  cout << "isItStormy: " << ec.isItStormy() << endl;
+
+  return 0;
 }
