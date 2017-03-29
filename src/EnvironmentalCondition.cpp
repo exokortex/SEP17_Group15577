69ff6b8086869df4848bc64bd4a45caa08028482
Verena <Verena@UNICORN>
2017-03-29 22:09:03 +0200

    try again

--- src/EnvironmentalCondition.cpp
+++ src/EnvironmentalCondition.cpp
@@ -11,6 +11,7 @@
 
 #include "EnvironmentalCondition.h"
 
+//------------------------------------------------------------------------------
 EnvironmentalCondition::EnvironmentalCondition(Cover sky_cover,
     Rank precipitation, float temperature, Rank wind)
 {
@@ -20,17 +21,29 @@ EnvironmentalCondition::EnvironmentalCondition(Cover sky_cover,
   this->wind_ = wind;
 }
 
+//------------------------------------------------------------------------------
+//methods with boolean value as return value (=true or false)
+//isItHot returns true if sky_cover is sunny and temperature is in the upper area
+//
 bool EnvironmentalCondition::isItHot()
 {
   return sky_cover_ == SUNNY && temperature_ >= TEMP_UPPERTHIRD;
 }
 
+//------------------------------------------------------------------------------
+// isItRainy returns true if precipitation is medium or
+// high and sky_cover is overcast or very overcast
+//
 bool EnvironmentalCondition::isItRainy()
 {
   return (precipitation_ == MEDIUM || precipitation_ == HIGH)
       && (sky_cover_ == OVERCAST || sky_cover_ == VERY_OVERCAST);
 }
 
+//------------------------------------------------------------------------------
+// isItStormy returns true if wind is medium or
+// high and sky_cover is overcast or very overcast
+//
 bool EnvironmentalCondition::isItStormy()
 {
   return (wind_ == MEDIUM || wind_ == HIGH)
