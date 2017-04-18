5949998b9e3d288b0ba901219d32ab33b8f429bd
Verena <Verena@UNICORN>
2017-04-18 10:42:10 +0200

    test

--- src/HTMLWriter.cpp
+++ src/HTMLWriter.cpp
@@ -1,10 +1,14 @@
-/*
- * HTMLWriter.cpp
- *
- *  Created on: 18.04.2017
- *      Author: Verena
- */
+//------------------------------------------------------------------------------
+// HTMLWriter.cpp
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
 
-
+#include "HTMLWriter.h"
 
 
