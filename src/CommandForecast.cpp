4c934ee9fd138228eb6a9f58c0d3cd69e77861ab
exokortex <johannes.haring@gmx.net>
2017-05-16 21:24:49 +0200

    forecast command created (does nothing and needs to be included in command list of handler)

---
+++ src/CommandForecast.cpp
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// CommandForecast.cpp
+//
+// Group: Group 15577, study assistant Roman Walch
+//
+// Authors: Johannes Haring 1611655
+//          Thomas Moder 1615103
+//          Verena Buder 1312723
+//------------------------------------------------------------------------------
+//
+
+#include <string>
+#include <sstream>
+
+#include "CommandForecast.h"
+#include "Command.h"
+#include "GameHandler.h"
+
+const string CommandForecast::NAME = "balance";
+const string CommandForecast::USAGE_STRING = "[ERR] Usage: balance\n";
+
+//------------------------------------------------------------------------------
+CommandForecast::CommandForecast() :
+    Command(NAME)
+{
+}
+
+//------------------------------------------------------------------------------
+int CommandForecast::execute(GameHandler& game, vector<string>& params)
+{
+}
